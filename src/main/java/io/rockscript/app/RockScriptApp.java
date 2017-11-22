2f8ab25f0d50a23ef87678a74a762e3603bc59b0
Tom Baeyens <mail@tombaeyens.be>
2017-11-22 21:28:32 +0100

    Refactoring (breaking build on branch!, wip)

--- src/main/java/io/rockscript/app/RockScriptApp.java
+++ src/main/java/io/rockscript/app/RockScriptApp.java
@@ -3,13 +3,9 @@
  */
 package io.rockscript.app;
 
-import io.rockscript.Server;
-
 public class RockScriptApp {
 
   public static void main(String[] args) {
-    new Server()
-      .parseArgs("server", "-p", "8080")
-      .execute();
+    throw new UnsupportedOperationException("TODO");
   }
 }
